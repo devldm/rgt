7739cba38627a645964c0ff9218c07389a29ad87
liam <39243060+devldm@users.noreply.github.com>
2024-03-05 18:38:55 +0900

    assignment 1

---
+++ app/data/products.ts
@@ -0,0 +1,19 @@
+import { product } from "@/types/types";
+
+export const products: product[] = [
+  {
+    productId: 1,
+    productName: "빨강",
+    productImageUrl: "/red.png"
+  },
+  {
+    productId: 2,
+    productName: "파랑",
+    productImageUrl: "/blue.png"
+  },
+  {
+    productId: 3,
+    productName: "녹색",
+    productImageUrl: "/green.png"
+  }
+]
---
+++ types/types.ts
@@ -0,0 +1,6 @@
+export type product = {
+  productId: number
+  productName: string
+  productImageUrl: string
+}
+
