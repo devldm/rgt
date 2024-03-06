f9c6c6a5f4cea08b4adc35396021a7145f9752cf
liam <39243060+devldm@users.noreply.github.com>
2024-03-06 22:19:08 +0900

    move files and update types

--- types/types.ts
+++ types/types.ts
@@ -1,6 +1,36 @@
 export type product = {
-  productId: number
-  productName: string
-  productImageUrl: string
-}
+  productId: number;
+  productName: string;
+  productImageUrl: string;
+};
 
+type productSales = {
+  A: number;
+  B: number;
+  C: number;
+};
+
+export type salesData = {
+  month: number;
+  productSales: productSales;
+};
+
+export type productScores = {
+  제품: string;
+  점수: number;
+};
+
+export type performanceIndicator = {
+  제품: string;
+  배터리수명: string;
+  처리속도: processingSpeed;
+};
+
+export type productData = {
+  제품: string;
+  점수: number;
+  배터리수명: string;
+  처리속도: processingSpeed;
+};
+
+type processingSpeed = "빠름" | "보통" | "느림";
